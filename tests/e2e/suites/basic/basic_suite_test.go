f86b24e11cf47396494b2188fc47fc8f75cb93e1
Nick Jüttner <nick@giantswarm.io>
2025-03-13 17:45:25 +0100

    testing

--- tests/e2e/suites/basic/basic_suite_test.go
+++ tests/e2e/suites/basic/basic_suite_test.go
@@ -1,13 +1,13 @@
 package basic
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
-	"github.com/giantswarm/clustertest/pkg/logger"
 	appsv1 "k8s.io/api/apps/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -15,6 +15,7 @@ import (
 	"github.com/giantswarm/apptest-framework/pkg/config"
 	"github.com/giantswarm/apptest-framework/pkg/state"
 	"github.com/giantswarm/apptest-framework/pkg/suite"
+	"github.com/giantswarm/clustertest/pkg/logger"
 )
 
 const (
@@ -36,32 +37,67 @@ func TestBasic(t *testing.T) {
 			// E.g. ensure that the initial install has completed and has settled before upgrading
 		}).
 		Tests(func() {
-			It("should run Nvidia validator", func() {
+			It("should have all GPU operator components running", func() {
 				wcClient, err := state.GetFramework().WC(state.GetCluster().Name)
 				Expect(err).Should(Succeed())
 
-				Eventually(func() bool {
-					return validatorDaemonSetReady(wcClient, "nvidia-operator-validator")
-				}).
-					WithTimeout(15*time.Minute).
-					WithPolling(15*time.Second).
-					Should(BeTrue(), "nvidia-operator-validator DaemonSet should be ready")
+				// Define components to check - name and type
+				components := map[string]string{
+					// DaemonSets
+					"nvidia-operator-validator":                  "daemonset",
+					"gpu-feature-discovery":                      "daemonset",
+					"nvidia-dcgm-exporter":                       "daemonset",
+					"nvidia-device-plugin-daemonset":             "daemonset",
+					"gpu-operator-node-feature-discovery-worker": "daemonset",
+
+					// Deployments
+					"gpu-operator": "deployment",
+					"gpu-operator-node-feature-discovery-master": "deployment",
+				}
+
+				// Check each component
+				for name, kind := range components {
+					By(fmt.Sprintf("Checking if %s is running", name))
+					var isReady bool
+
+					Eventually(func() bool {
+						if kind == "daemonset" {
+							isReady = validatorDaemonSetReady(wcClient, name)
+						} else if kind == "deployment" {
+							isReady = deploymentReady(wcClient, name)
+						}
+						return isReady
+					}).
+						WithTimeout(7*time.Minute).
+						WithPolling(15*time.Second).
+						Should(BeTrue(), fmt.Sprintf("%s should be ready", name))
+				}
 			})
 		}).
 		Run(t, "Basic Test")
 }
 
 func validatorDaemonSetReady(wcClient client.Client, daemonSetName string) bool {
+	logger.Log("Checking if %s DaemonSet is in running state", daemonSetName)
 	daemonSet := appsv1.DaemonSet{}
 	err := wcClient.Get(state.GetContext(), types.NamespacedName{Name: daemonSetName, Namespace: namespace}, &daemonSet)
 	if err != nil {
-		logger.Log("nvidia-operator-validator DaemonSet is not ready")
+		logger.Log("%s DaemonSet is not ready", daemonSetName)
 		return false
 	}
 
-	if daemonSet.Status.DesiredNumberScheduled == daemonSet.Status.NumberReady && daemonSet.Status.DesiredNumberScheduled > 0 {
-		logger.Log("nvidia-operator-validator DaemonSet is ready, desired number scheduled: %d, number ready: %d", daemonSet.Status.DesiredNumberScheduled, daemonSet.Status.NumberReady)
-		return true
+	return daemonSet.Status.DesiredNumberScheduled == daemonSet.Status.NumberReady && daemonSet.Status.DesiredNumberScheduled > 0
+}
+
+func deploymentReady(wcClient client.Client, deploymentName string) bool {
+	logger.Log("Checking if %s Deployment is in running state", deploymentName)
+	deployment := appsv1.Deployment{}
+	err := wcClient.Get(state.GetContext(), types.NamespacedName{Name: deploymentName, Namespace: namespace}, &deployment)
+	if err != nil {
+		logger.Log("%s Deployment is not ready", deploymentName)
+		return false
 	}
-	return false
+
+	// A deployment is considered ready when all of its replicas are ready
+	return deployment.Status.ReadyReplicas == deployment.Status.Replicas && deployment.Status.Replicas > 0
 }
